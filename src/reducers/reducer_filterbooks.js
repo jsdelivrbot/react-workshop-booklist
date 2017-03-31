6d04bfa2e08d2de1f70f336488a488a4d5070bfc
Raquel Sánchez <raquel.sanchez@atrapalo.com>
2017-03-31 21:33:32 +0200

    Modified structure states total and filter books

--- src/reducers/reducer_filterbooks.js
+++ src/reducers/reducer_filterbooks.js
@@ -2,7 +2,7 @@
 export default function (state = [], action){
     switch (action.type) {
         case 'FILTER_BOOKS':
-            return action.data;
+            return action.payload.data;
         default:  // Siempre un default en un switch pq el Reduce decide que hacer con cada acción
             return state;
     }
--- src/reducers/reducer_totalbooks.js
+++ src/reducers/reducer_totalbooks.js
@@ -1,7 +1,7 @@
 export default function (state = [], action){
     switch (action.type) {
         case 'TOTAL_BOOKS':
-            return action.data;
+            return action.payload.data;
         default:
             return state;
     }
