af0f3ae870f4c7eb56b7c98e841bd0598304e78e
Raquel Sánchez <raquel.sanchez@atrapalo.com>
2017-05-06 15:52:54 +0200

    Completed test for Paginator. Simulate onClick buttons

--- test/components/Paginator.test.js
+++ test/components/Paginator.test.js
@@ -1,6 +1,7 @@
 import {Paginator} from '../../src/components/Paginator';
 import React from 'react';
 import renderer from 'react-test-renderer';
+import _ from 'lodash';
 
 describe('Paginator component', () => {
 
@@ -15,7 +16,6 @@ describe('Paginator component', () => {
             elementsXPage: jest.fn()
         };
 
-
         let tree = renderer.create(
             <Paginator {...props} />
         ).toJSON();
@@ -23,4 +23,91 @@ describe('Paginator component', () => {
         expect(tree).toMatchSnapshot();
     });
 
+    it('It should render change next page correctly', () => {
+
+        let props = {
+            total_books: 300,
+            current_page: 1,
+            elements_x_page: 10,
+            totalBooks: jest.fn(),
+            currentPage: jest.fn(),
+            elementsXPage: jest.fn()
+        };
+
+        let tree = renderer.create(
+            <Paginator {...props} />
+        ).toJSON();
+
+        let moveToNextPage = tree.children[4];
+        moveToNextPage.props.onClick();
+
+        expect(_.flattenDeep(props.currentPage.mock.calls))
+            .toEqual([1, 2]);
+    });
+
+    it('It should render change previous page correctly', () => {
+
+        let props = {
+            total_books: 300,
+            current_page: 4,
+            elements_x_page: 10,
+            totalBooks: jest.fn(),
+            currentPage: jest.fn(),
+            elementsXPage: jest.fn()
+        };
+
+        let tree = renderer.create(
+            <Paginator {...props} />
+        ).toJSON();
+
+        let moveToPreviousPage = tree.children[2];
+        moveToPreviousPage.props.onClick();
+
+        expect(_.flattenDeep(props.currentPage.mock.calls))
+            .toEqual([1, 3]);
+    });
+
+    it('It should render change first page correctly', () => {
+
+        let props = {
+            total_books: 300,
+            current_page: 29,
+            elements_x_page: 10,
+            totalBooks: jest.fn(),
+            currentPage: jest.fn(),
+            elementsXPage: jest.fn()
+        };
+
+        let tree = renderer.create(
+            <Paginator {...props} />
+        ).toJSON();
+
+        let moveToFirstPage = tree.children[1];
+        moveToFirstPage.props.onClick();
+
+        expect(_.flattenDeep(props.currentPage.mock.calls))
+            .toEqual([1, 1]);
+    });
+
+    it('It should render change last page correctly', () => {
+
+        let props = {
+            total_books: 300,
+            current_page: 5,
+            elements_x_page: 10,
+            totalBooks: jest.fn(),
+            currentPage: jest.fn(),
+            elementsXPage: jest.fn()
+        };
+
+        let tree = renderer.create(
+            <Paginator {...props} />
+        ).toJSON();
+
+        let moveToLastPage = tree.children[5];
+        moveToLastPage.props.onClick();
+
+        expect(_.flattenDeep(props.currentPage.mock.calls))
+            .toEqual([1, 30]);
+    });
 });
